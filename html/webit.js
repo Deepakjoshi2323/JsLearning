186254c3c249dbe4e712472deafb6c2cc61673bd
Deepakjoshi2323 <155646244+Deepakjoshi2323@users.noreply.github.com>
2024-01-05 19:03:51 +0530

    Add files via upload

---
+++ html/webit.js
@@ -0,0 +1,526 @@
+//const profile ={
+    //usrname:"@erfvak_555sh",
+   /// isFollow: false,
+   // Followers:123,
+   // Following:123,
+
+
+//};
+
+//console.log(typeof profile["Following"]);
+
+
+
+//Artimetic operators
+//let a = 5;
+//let b = 8;
+
+
+// console.log ("a =",a,"& b =", b);
+// console.log ("a + b = ", a + b);
+// console.log ("a - b = ", a - b);
+// console.log ("a * b = ",a * b);
+// console.log ("a / b = ",a / b);
+// console.log ("a % b = ",a % b);
+// console.log ("a ** b = ",a ** b);
+
+
+//Unary operators
+//let a = 5;
+//let b = 8;
+
+
+
+
+
+//comparison operators
+//let a = 5;
+//let b = 2; 
+
+//console.log("6 >= 5", a >= b);  //true
+
+//conditional Statements
+
+//let age = 16;
+
+//if (age>= 18){
+    //console.log ("vote");
+//} else{
+  //  console.log ("not vote");
+//}
+
+
+
+
+
+
+
+//if (age >= 18){
+//   console.log ("you can vote");
+//}
+
+//if(age < 18) {
+ // console.log ("you CANNOT vote");
+//}
+
+
+// odd or even
+
+//let num = 30;
+
+//if (num % 2 === 0) {
+    //console.log(num, " is even");
+//} else{
+    //console.log( num,"is odd");
+
+//}
+
+
+
+
+ //let num = prompt ("enter a number:" );
+
+ //if (num % 5 === 0) {
+   // console.log(num,"is a multiple of 5");
+ //}else{
+  //  console.log (num, "is not a multiple of 5");
+ //}
+
+
+ //practice Qs2 --javascript
+
+ //let score = 11;
+ //let grade;
+ //if (score>=90 && score <= 100) {
+  //  grade = "A";
+ //}else if (score >=70 && score <= 89){
+ //   grade ="B";
+ //} else if (score >=60  && score <= 69 ){
+  //  grade = "c";
+// } else if (score >=50 && score <= 59 ){
+//    grade = "d";
+ //} else if (score >=0 && score <= 49 ){
+   // grade ="F";
+ //}
+
+ //console.log ("according to your scores, your grade was:", grade)
+
+
+ 
+ //console.log(" world javascript");
+ //console.log(" world javascript");
+ //console.log(" world javascript");
+ //console.log(" world javascript");
+ //console.log(" world javascript");
+
+
+ //print 1 to 5
+//for (var i = 1; i <= 100 ; i++) {
+   // console.log("i =", i); 
+//}
+
+//let i = 20;
+//while (i <= 10000) {
+    //console.log("javascript");
+    //i++;
+//}
+
+//let i = 1;
+//do{
+    //console.log ("i=" ,i);
+   //i++; 
+//}while (i <=5 );
+ 
+
+//for-of loop
+
+//let str = "javascript";
+
+
+//let size = 0;
+//for (let val of str) {
+  //  console.log ("val=", val);
+  //  size++;
+//}
+
+
+//console.log ("string size =", size); //10
+
+
+// for in loop
+
+//let student = {
+ //   name: "shreyen iyer",
+ //   age: 28,
+ //   cgpa: 5.6,
+  //  ispass: true,
+//};
+
+//for (let key in student) {
+  //  console.log ("key=", key, "value=", student [key]);
+//}
+
+
+//practice Qs1
+//for (let num = 0; num<= 50; num++) {
+   // if (num % 6 === 0){
+        // even number
+       // console.log ("num=",num);
+   // }
+//}
+
+//practice Qs2
+//let gameNum = 25; 
+//let userNum = prompt ("Guess the game number : ");
+
+//while(userNum != gameNum ) { 
+    //userNum = prompt("You entered wrong number. Guess again : ");
+
+//}
+
+//console.log (" congratulations, you entered the right number");
+
+//strings
+
+//let str2 = "javascript";
+
+
+//console.log (str2[7] ); //e
+
+
+//let obj ={
+  //item: "pen",
+ // price:10,
+//};
+
+//let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+//console.log(output);
+
+
+//console.log ("the cost of",obj. item, "is",obj.price, "rupees");
+
+
+
+//Template Literals
+//let specialString = `This is a template literal ${1 + 2 + 3} `;
+//console.log(specialString);
+
+//Template Literals
+//let str = "javascript \t welcome"; //20
+//console.log (str.length);
+
+
+
+// if (true){
+//   let age = 18;
+//   console.log (age);
+// }
+
+
+
+
+// var name = "hemant";
+// console.log (name) ;
+
+
+// let name = "world";
+// console.log (name) ;
+
+//  const PI = 3.212;
+// const name = "javascript";
+// console.log (name) ;
+
+
+// var name = "deepak";
+// name = "javascript";
+// console.log (name)
+
+ 
+// let name = "deepak";
+//  name = "java";
+//  console.log (name)
+
+
+// const name = "deepak";
+//  name = "javascript";
+//  console.log(name);
+
+// function modJs (love){
+//   if (love){
+//     let name ="Deepak";
+//     const sname = "Joshi";
+//   }
+//   console.log ("My Name is "+ name + " "+ sname );
+
+// }
+//  modJs (true);
+
+
+//  function modJs (love){
+//   if (love){
+//        var name ="Deepak";
+//        var sname = "Joshi";
+//   }
+//   console.log ("My Name is "+ name + " "+ sname );
+// }
+//  modJs (true);
+
+
+// var whowillwinToday = "India";
+
+// if (true){
+//  var whowillwinToday = "New zealand";
+//   console.log(whowillwinToday);
+// }
+
+// console.log(whowillwinToday);
+
+
+
+// let whowillwinToday = "India";
+
+// if (true){
+//  let whowillwinToday = "New zealand";
+//   console.log(whowillwinToday);
+// }
+
+// console.log(whowillwinToday);
+
+
+
+// const whowillwinToday = "India";
+
+// if (true){
+//  const whowillwinToday = "New zealand";
+//   console.log(whowillwinToday);
+// }
+
+// console.log(whowillwinToday);
+
+
+// console.log("My name is vinod."+" my age is 21")
+
+// console.log ("my name is world. "+"my birth is sept")
+
+// for (var i = 1; i < 5; i++) {
+//   console.log(i)
+// }
+
+// console.log(i)
+
+
+// const x = 5
+// if (true){
+//   const x = 1
+// }
+
+// console.log (x)
+
+
+
+
+// const y = 7
+// if (true){
+//   const y = 4
+// }
+
+// console.log (y)
+
+// let f = 8
+// if (true ){
+//   let f = 6
+// }
+
+// console.log(f)
+
+
+// var g = 5
+// if (true){
+//   var g = 3
+// }
+
+// console.log(g)
+
+
+
+
+// let a = 10;
+// console.log(b);
+// let b = 100;
+
+
+// var x = 10
+// function getName(){
+//   console.log("hello javascript!");
+// }
+
+
+// getName();
+// console.log(x);
+// console.log (getName);
+
+
+
+// var x = 1;
+// a();
+// b();
+
+// function a(){
+//     var x = 10;
+//     console. log (x); 
+// }
+
+
+// function b (){
+//   var x = 100
+//   console.log (x);
+// }
+
+
+// var firstname = "Yahoo";
+// var firstname = "Baba";
+
+// console.log (firstname);
+ 
+// function start(){
+//   for (var i = 0; i < 5; i++) {
+//     if (true) {
+//       var color = 'blue';
+//     }
+//   }
+//    console.log(color);
+// }
+
+ // var => function scoped
+ // ES6 (ES2015): let, const=> block-scoped
+// start();
+
+// var color = 'red';
+//  var age = 18;
+// var usrname = india;
+
+// function sayHi (){
+//   console.log ('hi');
+// }
+// function sayjavascript (){
+//   console.log ('javascript');
+// }
+
+// if (true){
+//   var varvariable = 'This is true'
+// }
+
+// console.log (varvariable)
+
+// if (true){
+//  var letvarible = 'This is true'
+  
+// }
+
+// console.log(letvarible)
+
+
+// const constvar = {name:'babe'}
+// let letvar = 1
+ 
+// constvar.name = 'sally'
+// constvar.age = '50'
+// constvar.from = 'world'
+// constvar.say = 'javacript'
+
+
+
+// console.log(constvar)
+
+// if (true){
+//   var color ="purple"
+// }
+// console.log(color);
+
+
+let dog = "rusty"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
+
+
+
+ 
+
+ 
